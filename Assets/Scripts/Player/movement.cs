0e57f1ed1cc2382ac998e5518c27759c18f17743
Daniel Vitoriano <daniel.costaht2@gmail.com>
2023-04-18 22:39:44 -0300

    ajustada as animações do mixamo e refeito script de movimento

--- Assets/Scripts/Player/movement.cs
+++ Assets/Scripts/Player/movement.cs
@@ -5,87 +5,64 @@ using Cinemachine;
 
 public class movement : MonoBehaviour
 {
-
-    private CharacterController characterController;
-    private Animator anim;
-    private Vector3 inputVector;
-    private float speed = 2f;
-    private float speedBase;
+    /* variaveis */
+    private Vector3 movementDirection;
+    private float characterSpeed;
+    private const float runningSpeed = 8f;
+    private const float walkingSpeed = 2f;
+    private float characterJumpForce = 1000;
     private bool isRunning = false;
-    private CinemachineVirtualCamera  cm;
-    private bool isOnFloor = true;
 
-    private void Awake() {
+    /* components */
+    private CinemachineVirtualCamera characterCamera;
+    private Animator characterAnimator;
+    private CharacterController characterController;
+
+    void Awake(){
+        characterCamera = FindObjectOfType<CinemachineVirtualCamera>();
+        characterAnimator = GetComponent<Animator>();
         characterController = GetComponent<CharacterController>();
-        anim = GetComponent<Animator>();
-        speedBase = speed;
-        cm = FindObjectOfType<CinemachineVirtualCamera>();
     }
 
-    private void FixedUpdate() {
+    void FixedUpdate()
+    {
         Move();
         SetAnim();
-        Running();
-        Jump();
-    }
-
-    private void Running(){
-        float targetSpeed = isRunning ? speedBase * 5.0f : speedBase;
-        speed = Mathf.Lerp(speed, targetSpeed, Time.deltaTime * 3.0f);
-        
-        isRunning = Input.GetButton("Run");
-        anim.SetBool("isRunning", isRunning);
-
-    }
-
-    private bool IsGrounded() {
-        float distanceToGround = 0.1f;
-        RaycastHit hit;
-        if (Physics.Raycast(transform.position, -Vector3.up, out hit, distanceToGround + 0.1f)) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    private void Jump(){
-        isOnFloor = IsGrounded();
-        if(Input.GetAxis("Jump") == 1 && isOnFloor){
-            anim.SetBool("isJumping", true);
-        }else{
-            anim.SetBool("isJumping", false);
-        }
     }
 
     private void Move(){
-        inputVector.Set(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        inputVector.Normalize();
+        movementDirection = Vector3.zero;
+        movementDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        movementDirection.Normalize();
 
-        Vector3 cameraForward = cm.transform.forward;
-        cameraForward.y = 0f;
+        if (Input.GetButton("Run") && !isRunning && movementDirection.z > 0.5f)
+        {
+            isRunning = true;
+        }else if(Input.GetButton("Run") && isRunning && movementDirection.z > 0.5f){
+            isRunning = false;
+        }else if(isRunning && movementDirection.z < 0.5){
+            isRunning = false;
+        }
 
-        if(inputVector != Vector3.zero){
+        characterSpeed = isRunning ? runningSpeed : walkingSpeed;
+
+        if(movementDirection.x != 0 || movementDirection.z != 0){
+            Vector3 cameraForward = characterCamera.transform.forward;
+            cameraForward.y = 0f;
             transform.rotation = Quaternion.LookRotation(cameraForward) * Quaternion.Euler(0f, 0f, 0f);
         }
 
-        if(inputVector.z < 0){
-            isRunning = false;
-            characterController.Move(inputVector * (speed * 0.5f) * Time.fixedDeltaTime);
-            return;
-        }
-
-        if(inputVector.x > 0 && inputVector.x < 1){
-            isRunning = false;
-            characterController.Move(inputVector * (speed * 0.7f) * Time.fixedDeltaTime);
-            return;
-        }
-
-        Vector3 moveDirection = transform.rotation * inputVector;
-        characterController.Move(moveDirection * speed * Time.fixedDeltaTime);
+        Vector3 currentMovement = transform.rotation * movementDirection;
+        characterController.Move(currentMovement * Time.fixedDeltaTime * characterSpeed);
     }
 
     private void SetAnim(){
-        anim.SetFloat("X", inputVector.x);
-        anim.SetFloat("Z", inputVector.z);
+        characterAnimator.SetFloat("X", movementDirection.x);
+        characterAnimator.SetFloat("Z", movementDirection.z);
+
+        //characterAnimator.SetBool("isJumping", isJumping);
+
+        characterAnimator.SetBool("isRunning", isRunning);
     }
+
 }
