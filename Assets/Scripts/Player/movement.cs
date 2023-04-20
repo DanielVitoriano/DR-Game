214df89821d212b1ce827f74fad6983386364274
Daniel Vitoriano <daniel.costaht2@gmail.com>
2023-04-20 12:14:05 -0300

    ajustados movimento de pulo

--- Assets/Scripts/Player/movement.cs
+++ Assets/Scripts/Player/movement.cs
@@ -7,13 +7,15 @@ public class movement : MonoBehaviour
 {
     /* variaveis */
     private Vector3 movementDirection;
+    private Vector3 characterYVelocity;
     private float characterSpeed;
-    private const float runningSpeed = 6f;
+    private const float runningSpeed = 4f;
     private const float walkingSpeed = 1.5f;
-    private float characterJumpForce = 25;
+    private float characterJumpSpeed = 1.25f;
     private bool isRunning = false;
     private bool isJumping = false;
-    private float gravity = 9.81f;
+    private float gravity = -9.81f;
+    private bool isGrounded;
 
     /* components */
     private CinemachineVirtualCamera characterCamera;
@@ -33,44 +35,46 @@ public class movement : MonoBehaviour
     }
 
     private void Move(){
-    movementDirection = Vector3.zero;
-    movementDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-    movementDirection.Normalize();
+        isGrounded = characterController.isGrounded;
+        movementDirection = Vector3.zero;
+        movementDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        movementDirection.Normalize();
 
-    if (Input.GetButton("Run") && !isRunning && movementDirection.z > 0.5f)
-    {
-        isRunning = true;
-    }else if(Input.GetButton("Run") && isRunning && movementDirection.z > 0.5f){
-        isRunning = false;
-    }else if(isRunning && movementDirection.z < 0.5){
-        isRunning = false;
+        if (Input.GetButton("Run") && !isRunning && movementDirection.z > 0.5f)
+        {
+            isRunning = true;
+        }else if(Input.GetButton("Run") && isRunning && movementDirection.z > 0.5f){
+            isRunning = false;
+        }else if(isRunning && movementDirection.z < 0.5){
+            isRunning = false;
+        }
+
+        characterSpeed = isRunning ? runningSpeed : walkingSpeed;
+
+        if(movementDirection != Vector3.zero){
+            Vector3 cameraForward = characterCamera.transform.forward;
+            cameraForward.y = 0f;
+            transform.rotation = Quaternion.LookRotation(cameraForward) * Quaternion.Euler(0f, 0f, 0f);
+        }
+
+        Vector3 currentMovement = transform.rotation * movementDirection;
+        characterController.Move(currentMovement * Time.fixedDeltaTime * characterSpeed);
+
+        if(characterYVelocity.y < 0 && isGrounded){
+            characterYVelocity.y = 0f;
+        }
+
+        if(isGrounded && Input.GetButton("Jump")){
+            isJumping = true;
+            characterYVelocity.y += Mathf.Sqrt(characterJumpSpeed * -3f * gravity + characterSpeed);
+        }
+        else if(isGrounded && isJumping){
+            isJumping = false;
+        }
+
+        characterYVelocity.y += gravity * Time.fixedDeltaTime;
+        characterController.Move(characterYVelocity * Time.fixedDeltaTime * characterJumpSpeed);
     }
-
-    characterSpeed = isRunning ? runningSpeed : walkingSpeed;
-
-    if(movementDirection != Vector3.zero){
-        Vector3 cameraForward = characterCamera.transform.forward;
-        cameraForward.y = 0f;
-        transform.rotation = Quaternion.LookRotation(cameraForward) * Quaternion.Euler(0f, 0f, 0f);
-    }
-
-    Vector3 currentMovement = transform.rotation * movementDirection;
-    currentMovement.y = 0;
-
-    if(!isJumping && characterController.isGrounded && Input.GetButton("Jump")){
-        isJumping = true;
-        currentMovement.y += characterJumpForce;
-    }
-    else if(characterController.isGrounded && isJumping){
-        isJumping = false;
-    }
-
-    if (!characterController.isGrounded) {
-        currentMovement.y -= gravity * Time.fixedDeltaTime;
-    }
-        
-    characterController.Move(currentMovement * Time.fixedDeltaTime * characterSpeed);
-}
 
 
     private void SetAnim(){
